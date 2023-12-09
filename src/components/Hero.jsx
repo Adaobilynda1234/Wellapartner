fafe13ea81761d979e164f751f6cffbd72dbf054
Adaobilynda1234 <adaobilynda1234@gmail.com>
2023-12-09 19:07:46 +0100

    heror commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="container">
+      <div className="hero-section">
+        <h1>10X your Pharmacy revenue with </h1>
+        <h1 className="hero-section__coloured">Wellapartner</h1>
+      </div>
+      <div className="hero-section__subsection">
+        <p>
+          Simplify your pharmacy business and streamline your workflow
+          <br /> and enhance patient care with Wellapartner
+        </p>
+      </div>
+      <div className="button">
+        <a href="/pricing" className="btn">
+          Get started{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="17"
+            viewBox="0 0 16 17"
+            fill="none"
+          >
+            <path
+              d="M1 8.17432H15"
+              stroke="#344054"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M8 1.17432L15 8.17432L8 15.1743"
+              stroke="#344054"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./home.css";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div>
+      <Hero />
       <h1>home</h1>
     </div>
   );
