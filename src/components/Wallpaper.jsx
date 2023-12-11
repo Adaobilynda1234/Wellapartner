8ba73fcbc0e0b85042b27b6feb8ced2d1e1c7273
Adaobilynda1234 <adaobilynda1234@gmail.com>
2023-12-11 02:03:00 +0100

    wallpaper-correction commit

--- src/components/Wallpaper.jsx
+++ src/components/Wallpaper.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./wallpaper.css";
 import wellapartner from "../assets/Wellapartner Dashboard (1).png";
-import wallimage from "../assets/wallpaper.png";
+import wallimage from "../assets/Wallpaper.png";
 
 const Wallpaper = () => {
   return (
